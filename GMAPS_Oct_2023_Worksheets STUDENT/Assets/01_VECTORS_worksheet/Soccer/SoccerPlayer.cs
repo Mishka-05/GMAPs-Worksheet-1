2f4baa3ccb0b01ae9230f6911f95cf5ebb14fa4e
Mishka-05 <matthew.steven.taylor@gmail.com>
2023-10-25 22:30:49 +0800

    Question 7 completed
    Question 7d, 7e and 7f completed.

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
@@ -17,6 +17,7 @@ public class SoccerPlayer : MonoBehaviour
         if (IsCaptain)
         {
             ArrangePlayers();
+            FindMinimum();
         }
     }
 
@@ -36,28 +37,70 @@ public class SoccerPlayer : MonoBehaviour
             OtherPlayers[i].transform.position = position;
         }
     }
-
-    //float Magnitude(Vector3 vector)
-    //{
+    
+    void FindMinimum()
+    {
+        // Creating a list to store all the randomised heights
+        List<float> heights = new List<float>();
+        // For loop to generate all the heights
+        for (int i = 0; i < 10; i++)
+        {
+            float height = Random.Range(5f, 20f);
+            // Adding each height float into the list
+            heights.Add(height);
+            Debug.Log(height);
+        }
         
-    //}
+        // Using the System.Linq OrderBy function to arrange the list in ascending order
+        heights = heights.OrderBy(h => h).ToList();
+        Debug.Log("Sorted in ascending order:");
+        // Printing out the heights now in ascending order
+        foreach (float height in heights)
+        {
+            Debug.Log(height);
+        }
+    }
+    float Magnitude(Vector3 vector)
+    {
+        return (float)Mathf.Sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+    }
 
-    //Vector3 Normalise(Vector3 vector)
-    //{
-        
-    //}
+    Vector3 Normalise(Vector3 vector)
+    {
+        float magnitude = Magnitude(vector);
+        vector.x /= magnitude;
+        vector.y /= magnitude;
+        vector.z /= magnitude;
+        return vector;
+    }
 
-    //float Dot(Vector3 vectorA, Vector3 vectorB)
-    //{
-        
-    //}
+    float Dot(Vector3 vectorA, Vector3 vectorB)
+    {
+        return (vectorA.x * vectorB.x) + (vectorA.y * vectorB.y) + (vectorA.z * vectorB.z);
+    }
 
-    //SoccerPlayer FindClosestPlayerDot()
-    //{
-    //    SoccerPlayer closest = null;
+    SoccerPlayer FindClosestPlayerDot()
+    {
+        SoccerPlayer closest = null;
+        float minAngle = 180f;
 
-    //    return closest;
-    //}
+        for (int i = 0; i < OtherPlayers.Length; i++)
+        {
+            Vector3 toPlayer = OtherPlayers[i].transform.position - transform.position;
+            toPlayer = Normalise(toPlayer);
+
+            float dot = Dot(transform.forward, toPlayer);
+            float angle = Mathf.Acos(dot);
+            angle = angle * Mathf.Rad2Deg;
+
+            if (angle < minAngle)
+            {
+                minAngle = angle;
+                closest = OtherPlayers[i];
+            }
+        }
+        return closest;
+    }
 
     void DrawVectors()
     {
@@ -77,6 +120,14 @@ public class SoccerPlayer : MonoBehaviour
             transform.localRotation = Quaternion.AngleAxis(angle, Vector3.up);
             Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
             DrawVectors();
+
+            SoccerPlayer targetPlayer = FindClosestPlayerDot();
+            targetPlayer.GetComponent<Renderer>().material.color = Color.green;
+
+            foreach (SoccerPlayer other in OtherPlayers.Where(t => t != targetPlayer))
+            {
+                other.GetComponent<Renderer>().material.color = Color.white;
+            }
         }
     }
 }
