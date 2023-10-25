2c4d6b35ed06319e30c581c4ae2b076b7a501521
Mishka-05 <matthew.steven.taylor@gmail.com>
2023-10-25 20:28:49 +0800

    Question 6 completed
    Completed question 6b, 6c and 6d

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/Player.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,71 +8,48 @@ public class Player : MonoBehaviour
     public bool IsCaptain = true;
     public Player OtherPlayer;
 
-    //float Magnitude(Vector3 vector)
-    //{
-    //    // Your code here
-    //}
+    public float Magnitude(Vector3 vector)
+    {
+        return (float)Mathf.Sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+    }
 
-    //Vector3 Normalise(Vector3 vector)
-    //{
-    //    // Your code here
-    //}
+    Vector3 Normalise(Vector3 vector)
+    {
+        float magnitude = Magnitude(vector);
+        vector.x /= magnitude;
+        vector.y /= magnitude;
+        vector.z /= magnitude;
+        return vector;
+    }
 
-    //float Dot(Vector3 vectorA, Vector3 vectorB)
-    //{
-    //    // Your code here
-    //}
+    public float Dot(Vector3 vectorA, Vector3 vectorB)
+    {
+        return (vectorA.x * vectorB.x) + (vectorA.y * vectorB.y) + (vectorA.z * vectorB.z);
+    }
 
-    //float AngleToPlayer()
-    //{
-    //    // Steps to calculate the angle between the direction Captain is facing and 
-    //    // the direction from Captain to Other
-    //    // 1. A
-    //    // 2. B
-    //    // 3. Calculate the angle between the vectors from A and B
-    //    //    3.1 Calculate the dot product between A and B
-    //    //       3.1.1 Calculate the magnitude of the vector from Captain to Other (A)
-    //    //       3.1.2 Normalise the vector from Captain to Other
-    //    //       3.1.3 Calculate the dot product of the normalised vectors A and B
-    //    //    3.2 From the dot product, calculate the actual angle between A and B
-    //    //       3.2.1 Take the arc cosine (acos) of the dot product (because if 
-    //    //             both vector are normalised, the dot product equals the cos
-    //    //             of the angle between vectors A and B.
-    //    //       3.2.2 The acos angle returned is in radians. We must convert to
-    //    //             degrees.
-
-    //    // A
-    //    // Steps to draw the arrow that represents the vector from Captain to Other 
-    //    // 1. Find the vector from Captain to Other
-    //    //     1.1 Find the position of Captain (A -- from)
-    //    //     1.2 Find the position of Other (B -- to)
-    //    //     1.3 Calculate B-A to get the vector from A to B
-    //    // 2. Draw the arrow to represent visually the vector AB
-    //    //
-
-    //    // Your code here
-
-    //    // B
-    //    // Steps to draw an arrow that represents which direction Captain is facing
-    //    // 1. Find which vector to draw as an arrow
-    //    //    1.1 The transform.forward vector of Captain
-    //    // 2. Draw the arrow to represent visually the the transform.forward vector of Captain
-    //    DebugExtension.DebugArrow(transform.position, transform.forward, Color.blue);
-
-    //    // CALCULATING THE ANGLE
-
-    //    // Your code here
-    //    // ...
-
-    //    // Your code here
-    //}
+    float FindAngle(Vector3 vectorA, Vector3 vectorB)
+    {
+        return (float)Mathf.Acos(Dot(vectorA, vectorB) / (Magnitude(vectorA) * Magnitude(vectorB))) * Mathf.Rad2Deg;
+    }
 
     void Update()
     {
         if (IsCaptain)
         {
-            //float angle = // Your code here
-            //Debug.Log(angle);
+            // Question 6b
+            Vector3 directionVector = OtherPlayer.transform.position - transform.position;
+            DebugExtension.DebugArrow(transform.position, directionVector, Color.black);
+
+            // Question 6c
+            Vector3 captForward = Normalise(transform.forward);
+            DebugExtension.DebugArrow(transform.position, captForward, Color.blue);
+
+            // Question 6d
+            float captAngle = FindAngle(transform.forward, directionVector);
+            float otherAngle = FindAngle(-directionVector, transform.forward);
+            Debug.Log("Captain to Other player angle: " + captAngle);
+            Debug.Log("Other player to captain angle: " + otherAngle);
+            Debug.Log(captAngle + otherAngle);
         }
     }
 }
