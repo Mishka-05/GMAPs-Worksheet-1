a4bae8273b476cc622834fb8df745103982a2826
Mishka-05 <matthew.steven.taylor@gmail.com>
2023-10-25 20:58:33 +0800

    Question 7a, b and c completed
    Completed question 7a, 7b and 7c as well as additional challenge to arrange all players an equal distance and angle from the captain in the center.
    
    Captain is also the only one drawing vectors instead of all drawing vectors to each other.

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
@@ -5,16 +5,37 @@ using System.Linq;
 
 public class SoccerPlayer : MonoBehaviour
 {
-    //public bool IsCaptain = false;
-    //public SoccerPlayer[] OtherPlayers;
-    //public float rotationSpeed = 1f;
+    public bool IsCaptain = false;
+    public SoccerPlayer[] OtherPlayers;
+    public float rotationSpeed = 1f;
 
-    //float angle = 0f;
+    float angle = 0f;
 
-    //private void Start()
-    //{
-       
-    //}
+    private void Start()
+    {
+        OtherPlayers = FindObjectsOfType<SoccerPlayer>().Where(t  => t != this).ToArray();
+        if (IsCaptain)
+        {
+            ArrangePlayers();
+        }
+    }
+
+    void ArrangePlayers()
+    {
+        float radius = 10f;
+        float angleStep = 360.0f / OtherPlayers.Length;
+        for (int i = 0; i < OtherPlayers.Length; i++)
+        {
+            float angle = Mathf.Deg2Rad * i * angleStep;
+            float x = radius * Mathf.Cos(angle);
+            float z = radius * Mathf.Sin(angle);
+
+            Vector3 position = new Vector3(x, 0, z);
+            position += transform.position;
+
+            OtherPlayers[i].transform.position = position;
+        }
+    }
 
     //float Magnitude(Vector3 vector)
     //{
@@ -38,19 +59,26 @@ public class SoccerPlayer : MonoBehaviour
     //    return closest;
     //}
 
-    //void DrawVectors()
-    //{
-    //    foreach (SoccerPlayer other in OtherPlayers)
-    //    {
-    //        // Your code here
-    //        // ...
-    //    }
-    //}
+    void DrawVectors()
+    {
+        foreach (SoccerPlayer other in OtherPlayers)
+        {
+            Debug.DrawRay(transform.position, other.transform.position - transform.position, Color.black);
+            
+        }
+    }
 
-    //void Update()
-    //{
-
-    //}
+    void Update()
+    {
+        DebugExtension.DebugArrow(transform.position, transform.forward, Color.red);
+        if (IsCaptain)
+        {
+            angle += Input.GetAxisRaw("Horizontal") * rotationSpeed;
+            transform.localRotation = Quaternion.AngleAxis(angle, Vector3.up);
+            Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
+            DrawVectors();
+        }
+    }
 }
 
 
