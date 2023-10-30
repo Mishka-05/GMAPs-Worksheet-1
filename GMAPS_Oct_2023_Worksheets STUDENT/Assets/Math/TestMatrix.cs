b9aaafd3098d98180a7efbecc9631bc68b606d14
Mishka-05 <matthew.steven.taylor@gmail.com>
2023-10-30 15:25:59 +0800

    Completed question 2
    Completed question 2a, b, c, d and e

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/Math/TestMatrix.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/Math/TestMatrix.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class TestMatrix : MonoBehaviour
 {
+    [SerializeField] bool Qn2;
     private HMatrix2D mat = new HMatrix2D();
     // Start is called before the first frame update
     void Start()
     {
-        mat.SetIdentity();
-        mat.Print();
+        if (Qn2)
+        {
+            Question2();
+        }
+        /*mat.SetIdentity();
+        mat.Print();*/
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void Question2()
     {
-        
+        HMatrix2D mat1 = new HMatrix2D(
+            6, 3, 2,
+            1, 7, 5,
+            8, 9, 4);
+
+        HMatrix2D mat2 = new HMatrix2D(
+            9, 2, 0,
+            4, 3, 0,
+            10, 8, 0);
+
+        HVector2D vec1 = new HVector2D(1,1);
+
+        HMatrix2D resultMat = mat1 * mat2;
+        resultMat.Print();
     }
 }
